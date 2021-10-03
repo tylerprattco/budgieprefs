002076b67064d6a5545f01a8e33398c0c2edbb3c
tylerprattco <87670197+tylerprattco@users.noreply.github.com>
2021-10-03 01:05:16 -0600

    Update budgie.sh

--- budgie.sh
+++ budgie.sh
@@ -4,49 +4,49 @@ cd
 cd /home/tyler
 git clone https://aur.archlinux.org/yay.git
 cd yay
-su - tyler -c "makepkg -si"
+makepkg -si
 cd ..
-rm -r yay
+sudo rm -r yay
 
 #dconf and wget
-pacman -S dconf wget --noconfirm
+sudo pacman -S dconf wget --noconfirm
 
 #gdm
-pacman -S gdm --noconfirm
+sudo pacman -S gdm --noconfirm
 
 #budgie
-pacman -S --needed budgie-desktop network-manager-applet gnome-control-center budgie-desktop-view budgie-extras budgie-screensaver gnome-calculator gnome-tweaks firefox --noconfirm
+sudo pacman -S --needed budgie-desktop network-manager-applet gnome-control-center budgie-desktop-view budgie-extras budgie-screensaver gnome-calculator gnome-tweaks firefox --noconfirm
 
 #budgie extra (SU NOTE)
-su - tyler -c "yay -S budgie-clipboard-applet budgie-screenshot-applet --noconfirm"
+yay -S budgie-clipboard-applet budgie-screenshot-applet --noconfirm
 
 #themes and shit
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 cd WhiteSur-gtk-theme
-./install.sh
+sudo ./install.sh
 cd ..
-rm -r WhiteSur-gtk-theme
+sudo rm -r WhiteSur-gtk-theme
 
 git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 cd papirus-icon-theme
-./install.sh
+sudo ./install.sh
 cd ..
-rm -r papirus-icon-theme
+sudo rm -r papirus-icon-theme
 
 #budgie prefs
 git clone https://github.com/tylerprattco/budgieprefs.git
 cd budgieprefs
-su - tyler -c "dconf load / < full-backup"
+dconf load / < full-backup
 cd ..
-rm -r budgieprefs
+sudo rm -r budgieprefs
 
 #nautilus and other shit
-pacman -S nautilus gthumb gparted 7zip-bin btop --noconfirm
+sudo pacman -S nautilus gthumb gparted 7zip-bin btop --noconfirm
 
 
 #terminal
-pacman -S tilix tilda --noconfirm
+sudo pacman -S tilix tilda --noconfirm
 
 #enable gdm
-systemctl enable gdm
-systemctl start gdm
+sudo systemctl enable gdm
+sudo systemctl start gdm
